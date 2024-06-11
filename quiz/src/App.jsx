54397f33307b5f03cf943d63e0e68c43a901393e
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-06-11 09:47:49 -0300

    new procject

---
+++ quiz/src/App.jsx
@@ -0,0 +1,16 @@
+
+// components
+import Welcome from './components/Welcome'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Quiz de Programacão</h1>
+      <Welcome/>
+    </div>
+  )
+}
+
+export default App
---
+++ quiz/src/components/Welcome.jsx
@@ -0,0 +1,15 @@
+import Quiz from "../img/quiz.svg"
+import "./Welcome.css"
+
+const Welcome = () => {
+  return (
+    <div id="welcome">
+        <h2>Seja bem-vido</h2>
+        <p>Clique no botão abixo para começar:</p>
+        <button>Iniciar</button>
+        <img src={Quiz} alt="Início do Quiz" />
+    </div>
+  )
+}
+
+export default Welcome
