b2cbb18827f7852ffe4ac39daf824b095f9f9af1
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-06-11 14:55:28 -0300

    context

--- quiz/src/components/Welcome.jsx
+++ quiz/src/components/Welcome.jsx
@@ -5,8 +5,8 @@ const Welcome = () => {
   return (
     <div id="welcome">
         <h2>Seja bem-vido</h2>
-        <p>Clique no botão abixo para começar:</p>
-        <button>Iniciar</button>
+        <p>Clique no botão abixo para começar</p>
+        <button type="button">Iniciar</button>
         <img src={Quiz} alt="Início do Quiz" />
     </div>
   )
