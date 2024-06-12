c13477ffdc28b5a2078e035ab6f8056e55fc85bf
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-06-12 09:29:47 -0300

    commit

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,16 +1,20 @@
 // react, componentes, estáticos
 import { useContext } from "react"
+import { QuizContext } from "./context/quiz"
 
 import Welcome from './components/Welcome'
+import Questtions from './components/Questions'
 
 import './App.css'
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext)
 
   return (
     <div className="app">
       <h1>Quiz de Programacão</h1>
-      <Welcome/>
+      {quizState.gamesStage === "Start" && <Welcome/>}
+      {quizState.gamesStage === "Playing" && <Questtions/>}
     </div>
   )
 }
